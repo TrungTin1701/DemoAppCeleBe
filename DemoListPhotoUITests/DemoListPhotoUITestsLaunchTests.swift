fc0fa7bcb5f41226908aa2a431802ec010163089
Tín/Perry/서비스개발팀 <perry@celebe.io>
2023-10-23 13:58:13 +0700

    Initial Commit

---
+++ DemoListPhotoUITests/DemoListPhotoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  DemoListPhotoUITestsLaunchTests.swift
+//  DemoListPhotoUITests
+//
+//  Created by Tin/Perry/ServiceDev on 23/10/2023.
+//
+
+import XCTest
+
+final class DemoListPhotoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
